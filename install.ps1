27cfb56ee584564c69e1dbed11367eba28b209b9
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2024-12-14 23:25:27 +0100

    chore: added a command to install vs2022 with desktop C++ workload to the install.ps1 script
    It turns out that it is indeed required.

--- install.ps1
+++ install.ps1
@@ -82,12 +82,6 @@ if (-not (winget install -e --id OpenJS.NodeJS --accept-source-agreements --acce
     exit 1
 }
 
-# Install Microsoft Visual C++ 2015-2022 Redistributable (x64) using Winget
-if (-not (winget install -e --id Microsoft.VCRedist.2015+.x64 --accept-source-agreements --accept-package-agreements)) {
-    Write-Host "Failed to install Microsoft Visual C++ 2015-2022 Redistributable (x64)."
-    exit 1
-}
-
 # Install Rust (MSVC version) using Winget
 if (-not (winget install -e --id Rustlang.Rust.MSVC --accept-source-agreements --accept-package-agreements)) {
     Write-Host "Failed to install Rust."
@@ -97,6 +91,18 @@ if (-not (winget install -e --id Rustlang.Rust.MSVC --accept-source-agreements -
 # Install pnpm using Winget
 if (-not (winget install -e --id pnpm.pnpm --accept-source-agreements --accept-package-agreements)) {
     Write-Host "Failed to install pnpm."
+    exit 1
+}
+
+# Install Visual Studio 2022 Community with the desktop C++ workload using Winget
+if (-not (winget install Microsoft.VisualStudio.2022.Community --accept-source-agreements --accept-package-agreements --override "--wait --quiet --add ProductLang Pl-pl --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended")) {
+    Write-Host "Failed to install Visual Studio 2022 Community with the desktop C++ workload."
+    exit 1
+}
+
+# Install Microsoft Visual C++ 2015-2022 Redistributable (x64) using Winget
+if (-not (winget install -e --id Microsoft.VCRedist.2015+.x64 --accept-source-agreements --accept-package-agreements)) {
+    Write-Host "Failed to install Microsoft Visual C++ 2015-2022 Redistributable (x64)."
     exit 1
 }
 
