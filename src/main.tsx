ce56abfcc16a26ba2719a5f06af59164e4f5bbeb
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2025-01-16 00:15:01 +0100

    chore: removed WorkingModeProvider from main.tsx
    unused

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import RootLayout from "@/app/layout";
-import { WorkingModeProvider } from "@/lib/providers/WorkingModeProvider";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <WorkingModeProvider>
-            <RootLayout>
-                <App />
-            </RootLayout>
-        </WorkingModeProvider>
+        <RootLayout>
+            <App />
+        </RootLayout>
     </React.StrictMode>
 );
