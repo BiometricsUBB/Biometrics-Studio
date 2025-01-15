fa42a238b868480ba2045dee72fc5d7c479bf6d7
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2025-01-15 23:42:38 +0100

    feature: clear markings and labels after changing work mode
    there are marking types that will not be available in other working
    modes

--- src/components/menu/mode-menu.tsx
+++ src/components/menu/mode-menu.tsx
@@ -8,6 +8,9 @@ import {
 import { t } from "i18next";
 import { useWorkingMode } from "@/lib/providers/WorkingModeProvider";
 import { WORKING_MODE } from "@/views/selectMode";
+import { MarkingsStore } from "@/lib/stores/Markings";
+import { CANVAS_ID } from "@/components/pixi/canvas/hooks/useCanvasContext";
+import { GlobalStateStore } from "@/lib/stores/GlobalState";
 
 export function ModeMenu() {
     const { workingMode, setWorkingMode } = useWorkingMode();
@@ -21,7 +24,28 @@ export function ModeMenu() {
                         <MenubarCheckboxItem
                             key={mode}
                             checked={workingMode === mode}
-                            onCheckedChange={() => setWorkingMode(mode)}
+                            onCheckedChange={() => {
+                                if (workingMode === mode) {
+                                    return;
+                                }
+                                setWorkingMode(mode);
+
+                                MarkingsStore(
+                                    CANVAS_ID.LEFT
+                                ).actions.markings.reset();
+                                MarkingsStore(
+                                    CANVAS_ID.RIGHT
+                                ).actions.markings.reset();
+                                MarkingsStore(
+                                    CANVAS_ID.LEFT
+                                ).actions.labelGenerator.reset();
+                                MarkingsStore(
+                                    CANVAS_ID.RIGHT
+                                ).actions.labelGenerator.reset();
+                                GlobalStateStore.actions.lastAddedMarking.setLastAddedMarking(
+                                    null
+                                );
+                            }}
                         >
                             {t(mode, { ns: "modes" })}
                         </MenubarCheckboxItem>
