8957f327a45348f08714cab9af72e047e331b9ea
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2025-02-08 00:58:36 +0100

    fix: fixed window transparency on macOS
    it was causing freezes

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,4 @@
 use tauri::Manager;
-use tauri_plugin_window_state::StateFlags;
 
 #[tauri::command]
 async fn show_main_window_if_hidden(window: tauri::Window) {
@@ -29,7 +28,7 @@ async fn close_splashscreen_if_exists(window: tauri::Window) {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_window_state::Builder::new().build())
+        //.plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
@@ -40,14 +39,15 @@ pub fn run() {
                 .expect("no main window")
                 .set_focus();
         }))
-        .plugin(tauri_plugin_store::Builder::default().build())
-        .plugin(
-            tauri_plugin_window_state::Builder::default()
-                .with_state_flags(
-                    StateFlags::all() & !StateFlags::VISIBLE & !StateFlags::DECORATIONS,
-                )
-                .build(),
-        )
+// plugin(
+// tauri_plugin_window_state::Builder::default()
+// .with_state_flags(
+// StateFlags::all()
+// & !StateFlags::VISIBLE
+// & !StateFlags::DECORATIONS,
+//)
+// .build(),
+// )
         .invoke_handler(tauri::generate_handler![
             show_main_window_if_hidden,
             close_splashscreen_if_exists,
