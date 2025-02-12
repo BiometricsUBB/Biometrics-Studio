eceebcfe283322f10a6baa355e496ba55e4e2638
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2025-02-12 17:52:01 +0100

    fix: temporary hide a maximize toggle in the window-controls if user is not windows machine
    will fix that later when we find the root cause of this issue

--- src/components/menu/window-controls.tsx
+++ src/components/menu/window-controls.tsx
@@ -3,9 +3,11 @@ import { useCallback, useEffect, useState } from "react";
 import { Toggle } from "@/components/ui/toggle";
 import { Minus, Maximize, Minimize, X } from "lucide-react";
 import { ICON } from "@/lib/utils/const";
+import { platform } from "@tauri-apps/plugin-os";
 
 export function WindowControls() {
     const [isMaximized, setIsMaximized] = useState(false);
+    const currentPlatform = platform();
 
     useEffect(() => {
         const updateMaximized = async () => {
@@ -44,24 +46,26 @@ export function WindowControls() {
             >
                 <Minus size={ICON.SIZE / 1.5} strokeWidth={ICON.STROKE_WIDTH} />
             </Toggle>
-            <Toggle
-                pressed={false}
-                onMouseDown={e => e.stopPropagation()}
-                onDoubleClick={e => e.stopPropagation()}
-                onClickCapture={maximizeWindow}
-            >
-                {isMaximized ? (
-                    <Minimize
-                        size={ICON.SIZE / 1.5}
-                        strokeWidth={ICON.STROKE_WIDTH}
-                    />
-                ) : (
-                    <Maximize
-                        size={ICON.SIZE / 1.5}
-                        strokeWidth={ICON.STROKE_WIDTH}
-                    />
-                )}
-            </Toggle>
+            {currentPlatform === "windows" && (
+                <Toggle
+                    pressed={false}
+                    onMouseDown={e => e.stopPropagation()}
+                    onDoubleClick={e => e.stopPropagation()}
+                    onClickCapture={maximizeWindow}
+                >
+                    {isMaximized ? (
+                        <Minimize
+                            size={ICON.SIZE / 1.5}
+                            strokeWidth={ICON.STROKE_WIDTH}
+                        />
+                    ) : (
+                        <Maximize
+                            size={ICON.SIZE / 1.5}
+                            strokeWidth={ICON.STROKE_WIDTH}
+                        />
+                    )}
+                </Toggle>
+            )}
             <Toggle
                 pressed={false}
                 onMouseDown={e => e.stopPropagation()}
